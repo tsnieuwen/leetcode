4fb9644e7f12af4e50d695e4d677d78eb9d5757a
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-07-18 20:05:39 -0600

    Update to repl

--- repl.rb
+++ repl.rb
@@ -1,23 +1,39 @@
 def is_valid(stale, latest, otjson)
   stale_array = stale.chars
   action_index = 0
-  otjson.each do |command|
-    if command[:op] == "skip"
+  json = parse_json(otjson)
+  json.each do |command|
+    if !command[:op]
+      break
+    elsif command[:op] == "skip"
       action_index += (command[:count])
     elsif command[:op] == "delete"
       command[:count].times do
         stale_array.delete_at(action_index)
       end
+      action_index = stale_array.count
     elsif command[:op] == "insert"
       chars_array = command[:chars].chars
       stale_array.insert(action_index, chars_array)
       stale_array = stale_array.flatten
+      action_index = stale_array.count
     end
+    require "pry"; binding.pry
   end
   stale_array.join == latest
 end
 
+def parse_json(json_string)
+  array = []
+  hash = Hash.new
+
+
+  array << hash
+  hash = Hash.new
+
+end
+
 original = "Hello World"
-outcome = "Hello Tommy"
-otjson = [{op: 'skip', count: 5}, {op: 'delete', count: 6}, {op: 'insert', chars: ' Tommy'}]
+outcome = "Hello Tommy Nieuwenhuis"
+otjson = [{op: 'skip', count: 5}, {op: 'delete', count: 6}, {op: 'insert', chars: ' Tommy'}, {op: 'insert', chars: ' Nieuwenhuis'}]
 puts is_valid(original, outcome, otjson)
